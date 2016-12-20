61d5053e9cbff6c31973b7ebb7eb9f576705a754
Alex <cawfree@gmail.com>
2016-12-20 22:02:03 +0000

    added events

---
+++ com/github/cawfree/boilerplate/IGLEventListener.java
@@ -0,0 +1,12 @@
+package com.github.cawfree.boilerplate;
+
+import com.github.cawfree.boilerplate.IGL;
+
+public interface IGLEventListener<T extends IGL> {
+	/* Here we provide accessors to the conventional OpenGL events but provide access to the abstract boilerplate API. */
+	public void init(final T pGL);
+	public void reshape(final T pGL, final int pX, final int pY, final int pWidth, final int pHeight);
+	public void display(final T pGL);
+	public void dispose(final T pGL);
+	
+}
